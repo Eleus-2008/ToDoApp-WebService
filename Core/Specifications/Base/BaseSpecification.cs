1549cfc296fba022989f6e967224cbd08e4ec7d3
Anton Eliseev <anton2008.e@yandex.ru>
2020-11-25 11:37:14 +0400

    add basic specification classes

---
+++ Core/Specifications/Base/BaseSpecification.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.Specifications.Base
+{
+    public abstract class BaseSpecification<T> : ISpecification<T>
+    {
+        public Expression<Func<T, bool>> Criteria { get; }
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T, object>>>();
+
+        protected BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+
+        protected virtual void AddInclude(Expression<Func<T, object>> includeExpression)
+        {
+            Includes.Add(includeExpression);
+        }
+    }
+}
