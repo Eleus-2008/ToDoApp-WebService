1acfb3f64fc6696a30ea3c986d7e81bac0fd5d5e
Anton Eliseev <anton2008.e@yandex.ru>
2020-11-26 17:38:22 +0400

    add authentication controller

---
+++ ToDoAppWebService/Controllers/AuthenticationController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ToDoAppWebService.Models;
+using Task = Core.Entities.Task;
+
+namespace ToDoAppWebService.Controllers
+{
+    [ApiController]
+    [Route("api/authentication")]
+    public class AuthenticationController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public AuthenticationController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody]RegisterModel model)
+        {
+            var userExists = await _userManager.FindByNameAsync(model.Username);
+            if (userExists != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            var user = new User()
+            {
+                UserName = model.Username,
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login([FromBody]LoginModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
