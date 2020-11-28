0f8f827f84e56c2a87a65ab1a4f82cc56bd2e4e5
Anton Eliseev <anton2008.e@yandex.ru>
2020-11-28 22:28:11 +0400

    add data annotations to models

--- ToDoAppWebService/Models/LoginModel.cs
+++ ToDoAppWebService/Models/LoginModel.cs
@@ -1,8 +1,12 @@
-﻿namespace ToDoAppWebService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAppWebService.Models
 {
     public class LoginModel
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
--- ToDoAppWebService/Models/RegisterModel.cs
+++ ToDoAppWebService/Models/RegisterModel.cs
@@ -1,9 +1,16 @@
-﻿namespace ToDoAppWebService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAppWebService.Models
 {
     public class RegisterModel
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Should be a valid email")]
         public string Email { get; set; }
+        [Required]
+        [MinLength(8, ErrorMessage = "Should be minimum 8 characters")]
         public string Password { get; set; }
     }
 }
