df48bc9bd27d0f29730bd11242608efbf7f67627
Anton Eliseev <anton2008.e@yandex.ru>
2020-11-28 22:30:17 +0400

    add login method in authentication controller

--- ToDoAppWebService/Controllers/AuthenticationController.cs
+++ ToDoAppWebService/Controllers/AuthenticationController.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Core.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using ToDoAppWebService.Models;
 using Task = Core.Entities.Task;
 
@@ -13,10 +19,12 @@ namespace ToDoAppWebService.Controllers
     [Route("api/authentication")]
     public class AuthenticationController : Controller
     {
+        private readonly IConfiguration _configuration;
         private readonly UserManager<User> _userManager;
 
-        public AuthenticationController(UserManager<User> userManager)
+        public AuthenticationController(IConfiguration configuration, UserManager<User> userManager)
         {
+            _configuration = configuration;
             _userManager = userManager;
         }
 
@@ -48,7 +56,38 @@ namespace ToDoAppWebService.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login([FromBody]LoginModel model)
         {
-            throw new NotImplementedException();
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.Username);
+                if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+                {
+                    var authClaims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, user.UserName),
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                    };
+
+                    var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                    var token = new JwtSecurityToken(
+                        issuer: _configuration["JWT:ValidIssuer"],  
+                        audience: _configuration["JWT:ValidAudience"],
+                        expires: DateTime.Now.AddHours(3),
+                        claims: authClaims,
+                        signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                    return Ok(new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+                        expiration = token.ValidTo
+                    });
+                }
+
+                return Unauthorized();
+            }
+
+            return BadRequest();
         }
     }
 }
