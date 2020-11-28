00652864f15be9ce2c2f5057c047be6492133081
Anton Eliseev <anton2008.e@yandex.ru>
2020-11-28 22:27:32 +0400

    configure services for using jwt

--- ToDoAppWebService/Startup.cs
+++ ToDoAppWebService/Startup.cs
@@ -1,18 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Core.Entities;
 using Infrastructure.Data;
 using Infrastructure.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ToDoAppWebService
 {
@@ -31,7 +35,29 @@ namespace ToDoAppWebService
             services.AddControllers();
 
             services.AddDataLayer(Configuration);
-            services.AddIdentityCore<User>().AddEntityFrameworkStores<SqlServerContext>();
+            services.AddIdentityCore<User>()
+                .AddEntityFrameworkStores<SqlServerContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidAudience = Configuration["JWT:ValidAudience"],
+                        ValidIssuer = Configuration["JWT:ValidIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+                    };
+                });
 
         }
 
@@ -48,6 +74,7 @@ namespace ToDoAppWebService
             app.UseRouting();
 
             app.UseAuthentication();
+            app.UseAuthorization();
             
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
