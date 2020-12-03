20778b8290fbdd1fd6627d961d8d6ffff542fbe4
Anton Eliseev <anton2008.e@yandex.ru>
2020-12-03 16:18:19 +0400

    add service interfaces

---
+++ ToDoAppWebService/Interfaces/ITaskService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Core.Entities;
+
+namespace ToDoAppWebService.Interfaces
+{
+    public interface ITaskService
+    {
+        System.Threading.Tasks.Task<IEnumerable<Task>> GetAllUserTasks(User user);
+        
+        System.Threading.Tasks.Task AddUserTasks(User user, IEnumerable<Task> tasks);
+        System.Threading.Tasks.Task UpdateUserTasks(User user, IEnumerable<Task> tasks);
+        System.Threading.Tasks.Task DeleteUserTasks(User user, IEnumerable<Task> tasks);
+    }
+}
---
+++ ToDoAppWebService/Interfaces/IToDoListService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Core.Entities;
+
+namespace ToDoAppWebService.Interfaces
+{
+    public interface IToDoListService
+    {
+        System.Threading.Tasks.Task<IEnumerable<ToDoList>> GetAllUserToDoLists(User user);
+        
+        System.Threading.Tasks.Task AddUserToDoLists(User user, IEnumerable<ToDoList> lists);
+        System.Threading.Tasks.Task UpdateUserToDoLists(User user, IEnumerable<ToDoList> lists);
+        System.Threading.Tasks.Task DeleteUserToDoLists(User user, IEnumerable<ToDoList> lists);
+    }
+}
