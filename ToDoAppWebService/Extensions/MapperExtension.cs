887f2b1ea9d2218ebd5a1c81b6d9b034e81ba3b0
Anton Eliseev <anton2008.e@yandex.ru>
2020-12-08 18:46:50 +0400

    add automapper

---
+++ ToDoAppWebService/Extensions/MapperExtension.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using ToDoAppWebService.Mapper;
+
+namespace ToDoAppWebService.Extensions
+{
+    public static class MapperExtension
+    {
+        public static IServiceCollection AddMapper(this IServiceCollection services)
+        {
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+            var mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+            return services;
+        }
+    }
+}
---
+++ ToDoAppWebService/Mapper/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core.Entities;
+using ToDoAppWebService.DTO;
+
+namespace ToDoAppWebService.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ToDoList, ToDoListDto>().ReverseMap();
+            CreateMap<Task, TaskDto>().ReverseMap();
+        }
+    }
+}
