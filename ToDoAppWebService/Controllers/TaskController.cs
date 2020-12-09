5e3a20a17ad76fa8a3ef5c9a85b3207a583b601b
Anton Eliseev <anton2008.e@yandex.ru>
2020-12-09 16:27:50 +0400

    add task controller implementation

---
+++ ToDoAppWebService/Controllers/TaskController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ToDoAppWebService.DTO;
+using ToDoAppWebService.Interfaces;
+
+namespace ToDoAppWebService.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/tasks/[action]")]
+    public class TaskController : Controller
+    {
+        private readonly ITaskService _taskService;
+        private readonly UserManager<User> _userManager;
+
+        public TaskController(ITaskService taskService, UserManager<User> userManager)
+        {
+            _taskService = taskService;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskDto>>> GetToDoLists()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> AddTasks([FromBody]IEnumerable<TaskDto> tasks)
+        {
+            var currentUser = await _userManager.FindByNameAsync(_userManager.GetUserName(User));
+            await _taskService.AddUserTasks(currentUser ,tasks);
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateTasks([FromBody]IEnumerable<TaskDto> tasks)
+        {
+            var currentUser = await _userManager.FindByNameAsync(_userManager.GetUserName(User));
+            await _taskService.UpdateUserTasks(currentUser ,tasks);
+            return Ok();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> DeleteTasks([FromBody]IEnumerable<Guid> tasksGuids)
+        {
+            var currentUser = await _userManager.FindByNameAsync(_userManager.GetUserName(User));
+            await _taskService.DeleteUserTasks(currentUser ,tasksGuids);
+            return Ok();
+        }
+    }
+}
