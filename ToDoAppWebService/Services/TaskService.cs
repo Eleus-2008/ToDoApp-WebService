f1077e0633301766924ca01716aaf2e5b1fa9cab
Anton Eliseev <anton2008.e@yandex.ru>
2020-12-26 22:06:20 +0400

    implement update and delete methods in task service

--- ToDoAppWebService/Services/TaskService.cs
+++ ToDoAppWebService/Services/TaskService.cs
@@ -48,38 +48,36 @@ namespace ToDoAppWebService.Services
 
         public async System.Threading.Tasks.Task UpdateUserTasks(User user, IEnumerable<TaskDto> tasksDtos)
         {
-            /*var tasks = _mapper.Map<IEnumerable<Task>>(tasksDtos).ToList();
-            var updatingTasks = tasks.Where(e =>
+            var tasks = _mapper.Map<IEnumerable<Task>>(tasksDtos).ToList();
+            foreach (var task in tasks)
             {
-                var list = _toDoListRepository.GetByIdAsync(e.ToDoListId).Result;
-                if (list == null || list.UserId == user.Id)
+                var currentTask = await _taskRepository.GetByIdAsync(task.Id);
+                if (currentTask != null && (await _toDoListRepository.GetByIdAsync(currentTask.ToDoListId)).UserId == user.Id)
                 {
-                    return true;
-                }
+                    currentTask.Name = task.Name;
+                    currentTask.IsDone = task.IsDone;
+                    currentTask.Priority = task.Priority;
+                    currentTask.Date = task.Date;
+                    currentTask.TimeOfBeginning = task.TimeOfBeginning;
+                    currentTask.TimeOfEnd = task.TimeOfEnd;
+                    currentTask.RepeatingConditions = currentTask.RepeatingConditions;
 
-                return false;
-            });
-
-            var updatingTasksEntities = new List<Task>();
-            foreach (var task in updatingTasks)
-            {
-                var currentEntity = await _taskRepository.GetByIdAsync(task.Id);
-                if (currentEntity != null)
-                {
-                    currentEntity
+                    await _taskRepository.UpdateAsync(currentTask);
                 }
             }
-            
-            await _taskRepository.UpdateRangeAsync(updatingTasks);*/
-            
-            throw new NotImplementedException();
         }
 
         public async System.Threading.Tasks.Task DeleteUserTasks(User user, IEnumerable<Guid> tasksGuids)
         {
-            /*var deletingLists =
-                await _taskRepository.GetAsync(e => tasksGuids.Any(id => id == e.Id) && _toDoListRepository.GetByIdAsync(e.ToDoListId).Result.UserId == user.Id);
-            await _taskRepository.DeleteRangeAsync(deletingLists);*/
+            foreach (var id in tasksGuids)
+            {
+                var currentTask = await _taskRepository.GetByIdAsync(id);
+                var canDelete = (await _toDoListRepository.GetByIdAsync(currentTask.ToDoListId)).UserId == user.Id;
+                if (canDelete)
+                {
+                    await _taskRepository.DeleteAsync(currentTask);
+                }
+            }
         }
     }
 }
