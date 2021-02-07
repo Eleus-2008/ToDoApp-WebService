174a5dd2ef4f574aa059ef6e517596b6d328034e
Anton Eliseev <anton2008.e@yandex.ru>
2021-02-07 21:07:40 +0400

    update specification

--- Core/Specifications/ToDoListsWithTasksByUserSpecification.cs
+++ Core/Specifications/ToDoListsWithTasksByUserSpecification.cs
@@ -5,7 +5,7 @@ namespace Core.Specifications
 {
     public class ToDoListsWithTasksByUserSpecification : BaseSpecification<ToDoList>
     {
-        public ToDoListsWithTasksByUserSpecification(User user) : base(e => e.User == user)
+        public ToDoListsWithTasksByUserSpecification(User user) : base(e => e.User == user && !e.IsDeleted)
         {
             AddInclude(e => e.Tasks);
         }
