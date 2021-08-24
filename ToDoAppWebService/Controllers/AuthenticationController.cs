b45a4246dced2501f4becf2455d5b4d4f9c9b999
Anton Eliseev <anton2008.e@yandex.ru>
2021-08-24 13:31:03 +0400

    add logging

--- ToDoAppWebService/Controllers/AuthenticationController.cs
+++ ToDoAppWebService/Controllers/AuthenticationController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using ToDoAppWebService.DTO;
 
@@ -20,19 +21,23 @@ namespace ToDoAppWebService.Controllers
     {
         private readonly IConfiguration _configuration;
         private readonly UserManager<User> _userManager;
+        private readonly ILogger _logger;
 
-        public AuthenticationController(IConfiguration configuration, UserManager<User> userManager)
+        public AuthenticationController(IConfiguration configuration, UserManager<User> userManager,
+            ILogger<AuthenticationController> logger)
         {
             _configuration = configuration;
             _userManager = userManager;
+            _logger = logger;
         }
 
         [HttpPost]
-        public async Task<IActionResult> Register([FromBody]RegisterDto dto)
+        public async Task<IActionResult> Register([FromBody] RegisterDto dto)
         {
             var userExists = await _userManager.FindByNameAsync(dto.Username);
             if (userExists != null)
             {
+                _logger.LogInformation("Ошибка регистрации пользователя: пользователь уже существует");
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
@@ -45,16 +50,18 @@ namespace ToDoAppWebService.Controllers
             var result = await _userManager.CreateAsync(user, dto.Password);
             if (!result.Succeeded)
             {
+                _logger.LogWarning("Ошибка регистрации пользователя: серверная ошибка");
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
+            _logger.LogInformation("Зарегистрирован новый пользователь {UserName}", user.UserName);
             return Ok();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Login([FromBody]LoginDto dto)
+        public async Task<IActionResult> Login([FromBody] LoginDto dto)
         {
-            var user = await _userManager.FindByNameAsync(dto.Username); 
+            var user = await _userManager.FindByNameAsync(dto.Username);
             if (user != null && await _userManager.CheckPasswordAsync(user, dto.Password))
             {
                 var authClaims = new List<Claim>
@@ -66,13 +73,15 @@ namespace ToDoAppWebService.Controllers
                 var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
 
                 var token = new JwtSecurityToken(
-                    issuer: _configuration["JWT:ValidIssuer"],  
+                    issuer: _configuration["JWT:ValidIssuer"],
                     audience: _configuration["JWT:ValidAudience"],
                     expires: DateTime.Now.AddHours(3),
                     claims: authClaims,
                     signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
                 );
 
+                _logger.LogInformation("Выдан токен пользователю {Username}, действительный до {ExpirationTime}",
+                    user.UserName, DateTime.Now.AddHours(3));
                 return Ok(new
                 {
                     token = new JwtSecurityTokenHandler().WriteToken(token),
@@ -80,6 +89,7 @@ namespace ToDoAppWebService.Controllers
                 });
             }
 
+            _logger.LogInformation("Неуспешная попытка аутентификации под логином {Username}", dto.Username);
             return Unauthorized();
         }
     }
